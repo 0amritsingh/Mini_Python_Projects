880094a385ce9983df765d4cd45feb61d9f6c429
Amrit Singh <amritsingh72007@gmail.com>
2024-08-08 09:48:13 +0530

    Update 017 Stack Implementation in Python.py

--- 017 Stack Implementation in Python.py
+++ 017 Stack Implementation in Python.py
@@ -14,12 +14,12 @@ def pop():
     more = input('Pop more? (y/n): ')
     pop() if more in 'yY' else None
 def peek():
-    print('\nStack is Empty!') if len(stack) == 0 else print(f'Peeked Item: {stack[-1]}')
-    more = input('Peek more? (y/n): ')
-    peek() if more in 'yY' else None
+    print('\nStack is Empty!') if len(stack) == 0 else print(f'Top Item: {stack[-1]}')
+def size():
+    print(f'\nSize of the Stack: {len(stack)}')
 while True:
-    user = input('\n++++++++++++++++++++\nPress [1]: Push\nPress [2]: Pop\nPress [3]: Peek\nPress [4]: Display\nPress [5]: Exit\n~> ')
-    push() if user == '1' else pop() if user == '2' else peek() if user == '3' else print(f'\nStack: {stack}') if user == '4' else exit() if user == '5' else print('\nERROR 101: Wrong input!')
+    user = input('\n____________________\nPress [1]: Push\nPress [2]: Pop\nPress [3]: Peek\nPress [4]: Display\nPress [5]: Size\nPress [6]: Exit\n~> ')
+    push() if user == '1' else pop() if user == '2' else peek() if user == '3' else print(f'\nStack: {stack}') if user == '4' else size() if user == '5' else exit() if user == '6' else print('\nERROR 101: Wrong input!')
 
 # online study matrial(theory/concept) of Stack: https://www.geeksforgeeks.org/stack-in-python
  
